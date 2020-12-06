c2626a99e2b74915d64ffd69cc280ecfc8961f20
Greg <remph819@gmail.com>
2020-12-06 06:11:37 -0700

    added Form

---
+++ client/src/actions/posts.js
@@ -0,0 +1,12 @@
+import {FETCH_ALL} from "../constants/actionTypes";
+
+import * as api from '../api'
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const {data} = await api.fetchPosts()
+        dispatch({type: FETCH_ALL, payload: data})
+    }catch (e) {
+        console.log(e)
+    }
+}
