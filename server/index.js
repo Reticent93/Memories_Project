9d4db62ac349ccdd10e8d947945a173f6068423c
Greg <remph819@gmail.com>
2020-12-05 20:00:17 -0700

    add index.js files in client and server folders

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const  mongoose = require('mongoose')
+
+
+const app = express()
+
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Memories API")
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((err) => console.log(err.message))
+
+mongoose.set('useFindAndModify', false)
