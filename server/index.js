693318233581a8ec8004aa20e280191cf09ccfbe
Greg <remph819@gmail.com>
2020-12-05 20:21:23 -0700

    another commit

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const  mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
